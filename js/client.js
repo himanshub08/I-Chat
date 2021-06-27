6db04e931129eeeaf4d24d2ce358caf6505cae10
himansh <himanshubhatnagar2@gmail.com>
2021-06-27 12:11:05 +0530

    I-Chat application

---
+++ js/client.js
@@ -0,0 +1,42 @@
+let socket=io('http://localhost:8000')
+const form=document.getElementById('send-container')
+const messageInput=document.getElementById('messageInp')
+const messageContainer=document.querySelector('.container')
+
+var audio= new Audio('ting.mp3')
+
+const append=(message,position)=>{
+    const messageElement=document.createElement('div')
+    messageElement.innerText=message
+    messageElement.classList.add('message')
+    messageElement.classList.add(position)
+    messageContainer.append(messageElement)
+    if(position=='left'){
+        audio.play()
+    }
+
+}
+form.addEventListener('submit',(e)=>{
+  e.preventDefault()
+   const message= messageInput.value
+   append(`${message}`,'right')
+   socket.emit('send',message)
+  messageInput.value= ''
+})
+
+
+
+
+let name=prompt("enter your name:")
+socket.emit('new-user-joined',name)
+
+socket.on("user-joined", name=>{
+    append(`${name} joined the chat`,'right')
+})
+
+socket.on('receive',data=>{
+    append(`${data.name}: ${data.message}`,'left')
+})
+socket.on('left',name=>{
+    append(`${name} left the chat`,'left')
+})
